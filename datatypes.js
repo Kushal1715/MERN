085732e22c3dffd46924454b24feccff1210ef3a
Kushal1715 <kushalkhadka77@gmail.com>
2023-12-05 06:47:33 +0545

    learning mernstack

---
+++ datatypes.js
@@ -0,0 +1,64 @@
+// Datatypes refers to what kind/type of value a variable can hold
+// Since JS is dynamically type programming languagethe type of value hold by a variable can change during runtime.
+// In JS, there are two major classification of datatypes:
+
+//1) Primitive data types: These data types hold a value directly within their variable and do not 
+// need to be referenced by another object. 
+//They are:
+
+// Number:
+// let num = 10;
+// let num2 = 2.35;
+// console.log(typeof num); (result in string)
+
+//String:
+//let address = "ktm";
+//let destination = "usa";
+
+//Boolean:
+//isAbove18 = true;
+
+//Undefined:
+//If a variable is declared but not assigned at that time, it holds undefined datatype.
+// let lastName;
+// let nationality = undefined; 
+
+//Null:
+//if a variable empty, it is known is null.
+// let age = null;
+
+//Symbol:
+// let id = Symbol(1);
+// let id2 = Symbol(1);
+// console.log(typeof id);
+// console.log(id === id2); false  
+
+//BigInt
+// let largerNum = 10n;
+
+//2) Non-primitive data types(Reference data types):These data types do not hold a value directly within their variable 
+// and need to be reference by another object  
+//They are:
+
+//Array:
+// let numbers = [1,5,3,6,63,2];
+// console.log(typeof numbers);
+
+//Object:
+// let obj1 = {
+//     name: 'kushal',
+//     age: 22
+// }
+// console.log(typeof obj1);
+
+// IMP
+// let arr1 =  [1,2,3];
+// let arr2 = arr1;
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
+
+// non-primitive data types cant be stored in callstack so heap memory is used
+
+
+
---
+++ variables.js
@@ -0,0 +1,68 @@
+//veriables - They are veru important concep in every programming language that are used to hold (store) data and manipulate them.
+//veriables can store valuas and expressions.
+//Expressions are something that will evantually resolves to a value.
+
+//Javascript is dynamically Typed programming language.
+
+//Syntax:
+//variable_type variable_name (identifier) = (assignment operator) value or expression.
+
+//There are three ways to declare a variables in javascript. They are:-
+//1. Var (Pre-ES6)
+//2. let (ES6+)
+//3. const (ES6+)
+
+//Rules for Identifier:
+//1. Identifier should always start with (a-z, A-Z, _, $)
+//2. It shouldn't start with number or special characters except($,_).
+//3. They are case sensative. It mean x and X are two different.
+//4. There shouldn't be space between the identifier.
+
+//Standard Conventions for Identifier in JS.
+//1. Variables names should be in camelCase (eg, var myName;)
+//2. Identifier must be descriptive and relevent to what kind of values it must hold(store).
+
+//Pascel Case (Every Word First Letter Capital) - oop
+
+//JavaScript is backward compatible pragramming language.
+
+//var vs let vs const
+//var - var is used to declare variable which value can or any change overtime in our program.
+//2. Variable decaration only possible in var.(eg. var firstName;)
+//var firstName; // value Decelaration 
+//firstName = "Kushal"; // value assign  
+//3. Variable re-decleration is possible in var.
+//4. Variables declared with var are hoisted.
+// Hoisting (only in var)
+// It is the default behaviour of javascript which raise the varirable declared with
+// var and function declaration to the top of its scope.
+// console.log(address);
+// var address = "ktm";
+// console.log(address);
+//5. Var is function scope.
+
+//let - let is used to declare variable which value can or any change overtime in our program.
+//2. Variable decaration only possible in let. (eg. let lastName;)
+//let lastName; // value decelaration 
+//lastName = "Khadka"; //value assign
+//3. Variable re-decleration is not possible in let.
+//4. Variables declared with let are not hoisted.
+//5. Let is block scope.
+
+//const - it is used to declare constant veriable.
+//2. Variable decaration only is not possible in Constant.(eg. const address;x)
+//3. Variable re-decleration is not possible in const.
+//4. Variables declared with const are not hoisted.
+//5. const is block scope.
+
+
+//Characteristics of variables in javascript
+//1. Mutability(change in origin eg: var and let)
+//2. Scope(region where the variable is declared.)
+//    i. Global scope
+//    ii.Local scope
+//        1.Function scope(region inside the curly bracket '{}' only in function)
+//        2.Block scope(region inside the curly bracket'{}')
+
+
+
